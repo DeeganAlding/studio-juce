8ea2b2d43b6927c62792dcc4190c3e816f7271d1
deeganalding <deeganalding@gmail.com>
2022-01-13 16:36:40 +1300

    Adding changes to help with passing audio to synthesizer

--- Synthesizer/Source/SynthVoice.h
+++ Synthesizer/Source/SynthVoice.h
@@ -17,7 +17,11 @@ class SynthVoice : public juce::SynthesiserVoice
 {
 public:
     bool canPlaySound(juce::SynthesiserSound* sound) override;
-    void StartNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition) override;
+    void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition) override;
+    void stopNote(float velocity, bool allowTailOff) override;
+    void controllerMoved(int controllerNumber, int newcControllerValue) override;
+    void pitchWheelMoved(int newPitchWheelValue) override;
+    void renderNextBlock(juce::AudioBuffer< float >& outputBuffer, int startSample, int numSamples) override;
 
 private:
 };
